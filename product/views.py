26bd92076abcbbde6181a2e644efefc8483e6493
Alex <ag.perez0500@gmail.com>
2022-09-30 14:42:26 -0500

    can see product details

--- product/views.py
+++ product/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from itertools import product
 from django.shortcuts import render
 
@@ -12,4 +13,15 @@ class LatestProductsList(APIView):
   def get(self, request, format=None):
     products = Product.objects.all()[0:4]
     serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+class ProductDetail(APIView):
+  def get_object(self, category_slug, product_slug):
+    try:
+      return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+    except Product.DoesNotExist:
+      raise Http404
+  def get(self, request, category_slug, product_slug, format=None):
+    product = self.get_object(category_slug, product_slug)
+    serializer = ProductSerializer(product)
     return Response(serializer.data)
