d809cec243aefc2f2aa6daa0b1629de708107568
Giolopez14 <glopez46@depaul.edu>
2022-11-06 17:56:45 -0600

    changed admin login, logout, and home page

--- trivia/admin.py
+++ trivia/admin.py
@@ -3,8 +3,19 @@ from django.contrib import admin
 from .models import Category, Trivia, Question, User, Favorite
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Trivia)
-admin.site.register(Question)
-admin.site.register(User)
-admin.site.register(Favorite)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Trivia)
+class TriviaAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    pass
+
