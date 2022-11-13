4b8235f43c3bab01395feead8d9a9364007c91c5
Alex <47337720+Hitook@users.noreply.github.com>
2022-11-13 09:49:47 -0600

    Setting up testing for matt

--- trivia/tests.py
+++ trivia/tests.py
@@ -2,6 +2,7 @@ from urllib import request
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import APIClient
+
 from .models import Trivia, Category
 from .serializer import TriviaSerializer, CategorySerializer, QuestionSerializer
 from rest_framework.response import Response
@@ -12,27 +13,39 @@ import collections
 
 # Create your tests here.
 class thing(TestCase):
-    def setup(self):
-        Trivia.objects.create(name='trivia', questions='1', slug='trivia', category_id='1', description='Hi')
-        Trivia.objects.create(name='math', questions='1', slug='math', category_id='2', description='Hi')
+    def setUp(self):
+      Category.objects.create(name='Math', slug='math')
+      Category.objects.create(name='Poetry', slug='poetry')
+      Trivia.objects.create(name='Trivia', questions='1', slug='trivia', category_id='1', description='Hi')
+      Trivia.objects.create(name='math', questions='1', slug='math', category_id='1', description='Hi')
 
     def test_trivia(self):
-      Category.objects.create(name='Math')
-      Category.objects.create(name='Poetry')
-      Trivia.objects.create(name='math', questions='1', slug='math', category_id='1', description='less annoying math')
-      Trivia.objects.create(name='algerba', questions='1', slug='algebra', category_id='1', description='annoying math')
-      Trivia.objects.create(name='poetry', questions='1', slug='poetry', category_id='2', description='Hi')
-
       client = APIClient()
-      response = client.get('/api/v1/latest-trivias/', format='json')
+
+      api_call = "/api/v1/trivias/{}/".format(1)
+      response = client.get(api_call, format='json')
       response = response.data
-      # print(type(response))
+
       for item in response:
-        print(item['get_absolute_url']) 
+        self.assertEquals(item['name'],'trivia')
 
-      # print(response.data)
-      # print(response.data[0])
+    def test_category(self):
+      client = APIClient()
 
-      # serializer = TriviaSerializer(trivias, many = True)
-      # data = Response(serializer.data)
-      # print(data)
+      api_call = "/api/v1/category/{}/".format(1)
+      response = client.get(api_call, format='json')
+      response = response.data
+
+      for item in response:
+        self.assertEquals(item['name'],'Math')
+
+    def test_categories(self):
+      client = APIClient()
+      api_call = "/api/v1/trivias/display-categories/"
+      response = client.get(api_call, format='json')
+      response = response.data
+
+      count = 0
+      for item in response:
+        count += 1
+      self.assertEquals(count, 2)
